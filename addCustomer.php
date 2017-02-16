bcc586f10f1b9418341c6671db4ad40e4fe71ca8
Vivienne Zhu <vivienne82394@gmail.com>
2017-02-15 21:43:02 -0500

    Add files via upload
    Create one that doesn't use AJAX and instead uses just PHP and a POST or a GET request

---
+++ addCustomer.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Last name: <?php echo $_POST["lastName"]; ?><br>
+First name: <?php echo $_POST["firstName"]; ?><br>
+Your phone number is: <?php echo $_POST["phone"]; ?>
+
+</body>
+</html>
